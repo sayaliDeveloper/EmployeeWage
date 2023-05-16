3cd3551c045d9dda8565835dd7388c3fee6a51eb
Uptech <uptech@trichemie.com>
2023-05-16 09:31:15 +0530

    Added:Java Program to calculate Wage of Part Time Employee

--- src/com/bridgelabz/wage/EmployeeWageProblem.java
+++ src/com/bridgelabz/wage/EmployeeWageProblem.java
@@ -5,10 +5,15 @@ public class EmployeeWageProblem {
         int is_Full_Time = 1;
         int empHour = 0;
         int empWageHr = 20;
-        double employee_check =Math.floor(Math.random () *10) % 2;
+        int empPartTime = 2;
+
+        double employee_check =Math.floor(Math.random () *10) % 3;
         if(employee_check==is_Full_Time){
             empHour = 8;
             System.out.println("Employee id Present");
+        }else if(employee_check == empPartTime){
+            empHour = 4;
+            System.out.println("Employee is Present as PartTime");
         }else {
             empHour = 0;
             System.out.println("Employee is Absent");
