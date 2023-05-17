45eac8be5c699a9f80f3b6f465fd091104649722
Uptech <uptech@trichemie.com>
2023-05-17 08:55:18 +0530

    Added: Java Program Using Switch Case

--- src/com/bridgelabz/wage/EmployeeWageProblem.java
+++ src/com/bridgelabz/wage/EmployeeWageProblem.java
@@ -3,12 +3,17 @@ package com.bridgelabz.wage;
 public class EmployeeWageProblem {
     public static void main(String[] args) {
         int is_Full_Time = 1;
-        int empHour = 0;
+        int empHour  = 0;
         int empWageHr = 20;
         int empPartTime = 2;
+      //  int totalWage = 0;
+      //  int totalHour = 0;
 
-        double employee_check =Math.floor(Math.random () *10) % 3;
-        if(employee_check==is_Full_Time){
+      //  for(int i=0;i<20;i++) {
+
+
+            int employee_check = (int)(Math.random() * 10) % 3;
+       /* if(employee_check==is_Full_Time){
             empHour = 8;
             System.out.println("Employee id Present");
         }else if(employee_check == empPartTime){
@@ -17,9 +22,29 @@ public class EmployeeWageProblem {
         }else {
             empHour = 0;
             System.out.println("Employee is Absent");
-        }
-        int wage = empHour*empWageHr;
-        System.out.println(wage);
+        }*/
+            switch (employee_check) {
+                case 1:
+                    empHour = 8;
+                    System.out.println("Employee id Present");
+                    break;
+                case 2:
+                    empHour = 4;
+                    System.out.println("Employee is Present as PartTime");
+                    break;
+                case 0:
+                    empHour = 0;
+                    System.out.println("Employee is Absent");
+                    break;
+            }
+            int wage = empHour * empWageHr;
+          //  totalHour = totalHour+empHour;
+         //   totalWage= totalWage+wage;
+            System.out.println(wage);
+        //}
+
+            //System.out.println(totalHour);
+            //System.out.println(totalWage);
 
     }
 }
