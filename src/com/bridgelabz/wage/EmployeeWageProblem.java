7fbaa0aaa9e8fc43decde9f8f3b50dfc226c8b96
Uptech <uptech@trichemie.com>
2023-05-26 00:12:58 +0530

    Added: Java Program to Calculate Employee wage Using Method

--- src/com/bridgelabz/wage/EmployeeWageProblem.java
+++ src/com/bridgelabz/wage/EmployeeWageProblem.java
@@ -1,11 +1,12 @@
 package com.bridgelabz.wage;
 
 public class EmployeeWageProblem {
-    public static void main(String[] args) {
-        int is_Full_Time = 1;
+   public static final int IS_FULL_TIME = 1;
+    public static final int EMP_PART_TIME = 2;
+    public static final int empWageHr = 20;
+
+    public static void calculateEmployeeWage(int empRate,int numDays,int MaxHours){
         int empHour  = 0;
-        int empWageHr = 20;
-        int empPartTime = 2;
         int totalWage = 0;
         int totalHour = 0;
 
@@ -13,16 +14,7 @@ public class EmployeeWageProblem {
 
 
             int employee_check = (int)(Math.random() * 10) % 3;
-       /* if(employee_check==is_Full_Time){
-            empHour = 8;
-            System.out.println("Employee id Present");
-        }else if(employee_check == empPartTime){
-            empHour = 4;
-            System.out.println("Employee is Present as PartTime");
-        }else {
-            empHour = 0;
-            System.out.println("Employee is Absent");
-        }*/
+
             switch (employee_check) {
                 case 1:
                     empHour = 8;
@@ -38,13 +30,21 @@ public class EmployeeWageProblem {
                     break;
             }
             int wage = empHour * empWageHr;
-           totalHour = totalHour+empHour;
-          totalWage= totalWage+wage;
+            totalHour = totalHour+empHour;
+            totalWage= totalWage+wage;
             System.out.println(wage);
         }
 
-            System.out.println(totalHour);
-            System.out.println(totalWage);
+        System.out.println(totalHour);
+        System.out.println(totalWage);
+
+
+    }
+
+
+    public static void main(String[] args) {
+         calculateEmployeeWage(20,2,10);
+
 
     }
 }
